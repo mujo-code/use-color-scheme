7ece45935067ff351443838e17f5b9b4b98f7a16
jcblw <jacoblowe2.0@gmail.com>
2019-06-15 09:04:24 -0700

    fix publish to make sure and publish al files

--- scripts/build.js
+++ scripts/build.js
@@ -1,6 +1,5 @@
 'use strict'
 
-const fs = require('fs')
 const rollup = require('rollup')
 const babel = require('rollup-plugin-babel')
 const { uglify } = require('rollup-plugin-uglify')
@@ -80,24 +79,5 @@ for (const config of bundles) {
       )
   )
 }
-
-// Copy package.json and LICENSE.txt
-promise = promise.then(() => {
-  delete pkg.private
-  delete pkg.devDependencies
-  delete pkg.scripts
-  delete pkg.eslintConfig
-  delete pkg.babel
-  fs.writeFileSync(
-    'dist/package.json',
-    JSON.stringify(pkg, null, '  '),
-    'utf-8'
-  )
-  fs.writeFileSync(
-    'dist/LICENSE.txt',
-    fs.readFileSync('LICENSE.txt', 'utf-8'),
-    'utf-8'
-  )
-})
 
 promise.catch(err => console.error(err.stack)) // eslint-disable-line no-console
